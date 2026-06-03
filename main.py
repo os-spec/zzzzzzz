f146b4c1b7d0086d2b66c71ccfa76fffaa517a48
os-spec <winoneoneoneone@gmail.com>
2026-06-03 03:46:10 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# Simple string
+print("Hello World")
+
+# Multiple arguments or variables
+user = "Alice"
+print("User logged in:", user)
+
+# Formatted strings (like JavaScript template literals)
+print(f"User {user} has connected.")
